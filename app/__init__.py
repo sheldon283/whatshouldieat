860c5fd8e3a9c451fcf02fa6c037947ccfd7e363
sheldon283 <dong.sheldon283@gmail.com>
2017-12-20 22:19:21 -0800

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
-from yelp import yelp
+from yelp import *
 
-y = yelp()
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,20 +17,57 @@ def homePost():
 
 
 
-	result = y.search(term, location)
+	result = search(term, location)
 	global reviews
 	if result.get('businesses') is None:
 		retValue = None
 		reviews = None
 	else:
-		retValue = y.query_api(result)
+		retValue = query_api(result)
 		if retValue is None:
 			reviews = None
 		else:
-			reviews=y.getReviews(retValue)
+			reviews=getReviews(retValue)
 
 	return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+# from flask import Flask, render_template, request
+# from yelp import yelp
+
+# y = yelp()
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template("home.html")
+
+# @app.route('/', methods=['POST'])
+# def homePost():
+# 	term = request.form["term"]
+# 	location = request.form["location"]
+
+# 	if not term or not location:
+# 		return render_template("home.html", result=None)
+
+
+
+# 	result = y.search(term, location)
+# 	global reviews
+# 	if result.get('businesses') is None:
+# 		retValue = None
+# 		reviews = None
+# 	else:
+# 		retValue = y.query_api(result)
+# 		if retValue is None:
+# 			reviews = None
+# 		else:
+# 			reviews=y.getReviews(retValue)
+
+# 	return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
+
+# if __name__ == '__main__':
+# 	app.run(debug=True)
 
