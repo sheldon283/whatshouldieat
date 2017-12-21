3fd756f889a9976b743e47a41bf2cc3641b77e2a
sheldon283 <dong.sheldon283@gmail.com>
2017-12-20 21:54:40 -0800

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
-from yelp import search, query_api, getReviews
+import yelp
 
+Yelp = yelp
+y = Yelp.yelp()
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,20 +19,20 @@ def homePost():
 
 
 
-	# result = search(term, location)
-	# global reviews
-	# if result.get('businesses') is None:
-	# 	retValue = None
-	# 	reviews = None
-	# else:
-	# 	retValue = query_api(result)
-	# 	if retValue is None:
-	# 		reviews = None
-	# 	else:
-	# 		reviews=getReviews(retValue)
+	result = y.search(term, location)
+	global reviews
+	if result.get('businesses') is None:
+		retValue = None
+		reviews = None
+	else:
+		retValue = y.query_api(result)
+		if retValue is None:
+			reviews = None
+		else:
+			reviews=y.getReviews(retValue)
 
-	# return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
-	return "Hello"
+	return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
