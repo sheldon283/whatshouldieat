d3c8561d0a57e969e39a643e859e439fc029059b
sheldon283 <dong.sheldon283@gmail.com>
2017-12-22 14:28:27 -0800

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-import yelpLib	
+from yelpLib import test
 import sys
 
-print(yelpLib)
-sys.stdout.flush()
+# print(test)
+# sys.stdout.flush()
 app = Flask(__name__)
 
-yelpLib.test()
+test()
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -37,41 +37,3 @@ def homePost():
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
-# from flask import Flask, render_template, request
-# from yelp import yelp
-
-# y = yelp()
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
-
-# @app.route('/', methods=['POST'])
-# def homePost():
-# 	term = request.form["term"]
-# 	location = request.form["location"]
-
-# 	if not term or not location:
-# 		return render_template("home.html", result=None)
-
-
-
-# 	result = y.search(term, location)
-# 	global reviews
-# 	if result.get('businesses') is None:
-# 		retValue = None
-# 		reviews = None
-# 	else:
-# 		retValue = y.query_api(result)
-# 		if retValue is None:
-# 			reviews = None
-# 		else:
-# 			reviews=y.getReviews(retValue)
-
-# 	return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
-
-# if __name__ == '__main__':
-# 	app.run(debug=True)
-
