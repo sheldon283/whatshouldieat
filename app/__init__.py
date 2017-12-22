c9fec112c782141ce2a7fc1de1326f2aa21cb3c8
sheldon283 <dong.sheldon283@gmail.com>
2017-12-22 13:50:06 -0800

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-import yelp	
+import yelpLib	
 import sys
 
-print(yelp)
+print(yelpLib)
 sys.stdout.flush()
 app = Flask(__name__)
 
-yelp.test()
+yelpLib.test()
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -21,17 +21,17 @@ def homePost():
 
 
 
-	result = yelp.search(term, location)
+	result = yelpLib.search(term, location)
 	global reviews
 	if result.get('businesses') is None:
 		retValue = None
 		reviews = None
 	else:
-		retValue = yelp.query_api(result)
+		retValue = yelpLib.query_api(result)
 		if retValue is None:
 			reviews = None
 		else:
-			reviews=yelp.getReviews(retValue)
+			reviews=yelpLib.getReviews(retValue)
 
 	return render_template('item.html', object=result, result=retValue, reviews=reviews, text=term, location=location)
 
